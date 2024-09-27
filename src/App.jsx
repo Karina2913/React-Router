7f6be53a27553905f14fd08c6e33eddeb1a2fe54
Karina <karinapliegovergara@gmail.com>
2024-09-27 15:59:57 -0500

    updated navbar

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { Routes, Route, Link } from 'react-router-dom'
+import Blue from './components/Blue'
+import Red from './components/Red'
+import Home from './components/Home'
+import './App.css'
+
+export default function App () {
+  return (
+    <div id="container">
+      <div id="navbar">
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/blue" element={<Blue />} />
+          <Route path="/red" element={<Red />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  )
+}
