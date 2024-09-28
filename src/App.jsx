f9126462a2a2f60ddf89fedaf1b5e54365e39260
Karina <karinapliegovergara@gmail.com>
2024-09-28 10:31:56 -0500

    navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Blue from './components/Blue'
 import Red from './components/Red'
 import Home from './components/Home'
+import Navbar from './components/Navbar'
 import './App.css'
 
 export default function App () {
   return (
     <div id="container">
-      <div id="navbar">
-        <Link to="/blue">Blue</Link>
-        <Link to="/red">Red</Link>
-      </div>
+      <Navbar />
       <div id="main-section">
         <Routes>
           <Route path="/blue" element={<Blue />} />
